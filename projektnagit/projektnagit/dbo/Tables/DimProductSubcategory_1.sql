5d18d326e6343f0f8526ea2c9a9817c6621a0f5b
Natalia Helińska <natalia.w.helinska@gmail.com>
2019-07-29 18:47:06 +0200

    Nowe 18:46

---
+++ projektnagit/projektnagit/dbo/Tables/DimProductSubcategory_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DimProductSubcategory] (
+    [ProductSubcategoryKey]         INT            IDENTITY (1, 1) NOT NULL,
+    [ProductSubcategoryLabel]       NVARCHAR (100) NULL,
+    [ProductSubcategoryName]        NVARCHAR (50)  NOT NULL,
+    [ProductSubcategoryDescription] NVARCHAR (100) NULL,
+    [ProductCategoryKey]            INT            NULL
+);
+
